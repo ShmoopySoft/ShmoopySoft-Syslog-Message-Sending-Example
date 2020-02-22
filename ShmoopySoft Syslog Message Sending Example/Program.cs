4505619f8189e0e2fa93ce527d5942a404f60e3c
ShmoopySoft <info@shmoopysoft.co.za>
2020-02-22 10:34:18 +0200

    Add project files.

---
+++ ShmoopySoft Syslog Message Sending Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShmoopySoft_Syslog_Message_Sending_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
